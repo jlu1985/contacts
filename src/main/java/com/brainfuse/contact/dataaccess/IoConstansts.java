14e6a23f782422563c0c92702aba0bfe1ebbe6c1
jialu <jlu@brainfuse.com>
2015-06-15 17:28:06 -0400

    wrong name

--- src/main/java/com/brainfuse/contact/dataaccess/IoConstansts.java
+++ src/main/java/com/brainfuse/contact/dataaccess/IoConstansts.java
@@ -5,6 +5,6 @@ public interface IoConstansts {
 	public final static String BASIC_ACCESS_CONTACT_NAME = "contactDao";
 	public final static String BASIC_ACCESS_RELATIONSHIP_NAME = "relationshipDao";
 	public final static String BASIC_ACCESS_CITY_NAME = "cityDao";
-	public final static String BASIC_ACCESS_STATE_NAME = "contactDao";
+	public final static String BASIC_ACCESS_STATE_NAME = "stateDao";
 	
 }
